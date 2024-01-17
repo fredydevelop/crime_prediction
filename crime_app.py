55e768f9d410293c230f5299fc3f9bfaa4f098fe
Coded Coder <105555015+fredydevelop@users.noreply.github.com>
2024-01-17 05:07:53 +0100

    Update crime_app.py

--- crime_app.py
+++ crime_app.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
-from sklearn.metrics import accuracy_score,precision_score,f1_score,confusion_matrix,classification_report
+#import seaborn as sns
+#from sklearn.metrics import accuracy_score,precision_score,f1_score,confusion_matrix,classification_report
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 
 import streamlit as st 
 import base64
