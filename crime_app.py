586157bbf4bc7d578335b6a1c32f11d59cc3d294
Coded Coder <105555015+fredydevelop@users.noreply.github.com>
2024-08-15 12:06:50 +0100

    Update crime_app.py

--- crime_app.py
+++ crime_app.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title='Crime Prediction system',layout='centered')
 
 #selection=option_menu(menu_title="Main Menu",options=["Single Prediction","Multi Prediction"],icons=["cast","book","cast"],menu_icon="house",default_index=0)
 with st.sidebar:
-    st.image("IMG_5839-removebg-preview.png")
+    st.image("IMG_5839-removebg-preview.png",width=None, use_column_width="always")
     st.write()
     st.title("Home Page")
     selection=st.radio("select your option",options=["predict a Single Crime Case", "Predict for Multi-Criime cases"])
